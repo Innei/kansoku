0090729a92077f60be4ebbeb3cfa85f88b481490
Innei <tukon479@gmail.com>
2026-07-17 22:23:52 +0800

    chore(app): add dev:license-test and dev:unlocked scripts

--- app/web/src/pages/cockpit/chat/LockedChatDock.tsx
+++
@@ -1,9 +0,0 @@
-import { LockedAiNotice } from "../../LockedAiNotice";
-
-export function LockedChatDock() {
-  return (
-    <div className="chat-dock chat-dock--locked">
-      <LockedAiNotice message="追问功能需要有效授权" />
-    </div>
-  );
-}
